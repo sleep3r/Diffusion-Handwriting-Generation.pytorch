94e56ad9b011154691bf958ce2a4ce6f48b5536f
sleep3r <AYuryevikalashnikov@sberbank.ru>
2023-02-16 20:07:00 +0300

    Codestyle

--- diffusion_handwriting_generation/conditioning.py
+++ diffusion_handwriting_generation/conditioning.py
@@ -3,8 +3,6 @@ from torch import nn
 
 
 class AffineTransformLayer(nn.Module):
-    """Applies an affine transformation to the input tensor."""
-
     def __init__(self, hidden: int):
         super().__init__()
 
@@ -17,14 +15,6 @@ class AffineTransformLayer(nn.Module):
         self.gamma_emb.bias.data.fill_(1)
 
     def forward(self, x: torch.Tensor, sigma: torch.Tensor) -> torch.Tensor:
-        """
-        Args:
-            x (torch.Tensor): input tensor with shape (batch_size, filters, seq_len);
-            sigma (torch.Tensor): scaling parameters with shape (batch_size, seq_len).
-
-        Returns:
-            torch.Tensor: transformed tensor with shape (batch_size, filters, seq_len)
-        """
         gammas = self.gamma_emb(sigma)
         betas = self.beta_emb(sigma)
         return x * gammas + betas
