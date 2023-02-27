ac537745c2f9811794388596a265c6bfe0bba5d8
sleep3r <AYuryevikalashnikov@sberbank.ru>
2023-02-27 04:06:17 +0300

    Cfg fixes

--- diffusion_handwriting_generation/config.py
+++ diffusion_handwriting_generation/config.py
@@ -1,8 +1,7 @@
-# Standard Library
 import io
 from os import PathLike
 import pydoc
-from typing import Any, Optional
+from typing import Any, Union
 
 from addict import Dict
 from fire import Fire
@@ -35,11 +34,17 @@ class DLConfig:
         return self.__cfg[key]
 
     @classmethod
-    def load(cls, path: PathLike | str):
+    def load(cls, path: Union[PathLike, str]):
         yaml = YAML()
         with open(path, "r") as f:
             yaml_config = yaml.load(f)
         return cls(yaml_config)
+
+    def dump(self, path: PathLike):
+        yaml = YAML()
+
+        with open(path, "w") as f:
+            yaml.dump(self.__yaml_config, f)
 
     @property
     def pretty_text(self) -> str:
@@ -48,11 +53,19 @@ class DLConfig:
         yaml.dump(self.__yaml_config, buf)
         return buf.getvalue().decode("utf-8")
 
-    def dump(self, path: PathLike):
-        yaml = YAML()
 
-        with open(path, "w") as f:
-            yaml.dump(self.__yaml_config, f)
+def merge_configs(
+    base_cfg: CommentedMap, cfg: Union[CommentedMap, dict]
+) -> CommentedMap:
+    """Merges base config with the new one."""
+    for k, v in cfg.items():
+        if isinstance(v, dict):
+            if k not in base_cfg:
+                base_cfg[k] = {}
+            merge_configs(base_cfg[k], v)
+        else:
+            base_cfg[k] = v
+    return base_cfg
 
 
 def update_config(config: CommentedMap, params: dict) -> CommentedMap:
@@ -72,9 +85,10 @@ def update_config(config: CommentedMap, params: dict) -> CommentedMap:
     return config
 
 
-def fit(**kwargs) -> CommentedMap:
+def fit_config(**kwargs) -> CommentedMap:
     """
     Loads base config and updates it with specified new one --config with some others --params.
+    Also does inheritance from `base.yml`.
     """
     yaml = YAML()
 
@@ -84,47 +98,46 @@ def fit(**kwargs) -> CommentedMap:
     if "config" in kwargs:
         cfg_name = kwargs.pop("config")
         with open(f"./diffusion_handwriting_generation/configs/{cfg_name}", "r") as f:
-            cfg_yaml = yaml.load(f)
+            yaml_config = yaml.load(f)
 
-        merged_cfg = update_config(base_config, cfg_yaml)
+        merged_cfg = merge_configs(base_config, yaml_config)
     else:
         merged_cfg = base_config
-    update_cfg = update_config(merged_cfg, kwargs)
-    return update_cfg
+
+    updated_cfg = update_config(merged_cfg, kwargs)
+    return updated_cfg
 
 
-def object_from_dict(d: Optional[CfgDict], parent=None, **default_kwargs) -> Any:
+def object_from_dict(d: CfgDict, *args, **default_kwargs) -> Any:
     """Loads python object from cfg dict with params."""
     kwargs = dict(d).copy()
-
     object_type = kwargs.pop("type", None)
-    if object_type is not None:
-        params = kwargs.pop("params", None)
 
-        for name, value in default_kwargs.items():
-            params.setdefault(name, value)
+    if object_type is None:
+        raise ImportError(
+            "Can't initialize any object from dict without `type` key specified",
+            kwargs,
+        )
 
-        if parent is not None:
-            if params is not None:
-                return getattr(parent, object_type)(**params)
-            else:
-                return getattr(parent, object_type)
+    params = kwargs.pop("params", None)
+    for name, value in default_kwargs.items():
+        params.setdefault(name, value)
+
+    try:
+        if params is not None:
+            return pydoc.locate(object_type)(*args, **params)  # noqa
         else:
-            try:
-                if params is not None:
-                    return pydoc.locate(object_type)(**params)  # noqa
-                else:
-                    return pydoc.locate(object_type)
-            except TypeError:
-                raise ImportError(
-                    "Check module is accessible/installed and correct module params for",
-                    object_type,
-                )
+            return pydoc.locate(object_type)
+    except TypeError:
+        raise ImportError(
+            "Check module is accessible/installed and correct module params for",
+            object_type,
+        )
 
 
 def config_entrypoint() -> DLConfig:
-    yaml_config: CommentedMap = fit(
-        **Fire(lambda **kwargs: kwargs)
-    )  # dirty hack to prevent double print of cfg
+    """Loads config from shell arg path."""
+    # kwargs only read using fire to prevent double print of cfg
+    yaml_config = fit_config(**Fire(lambda **kwargs: kwargs))
     cfg = DLConfig(yaml_config)
     return cfg
